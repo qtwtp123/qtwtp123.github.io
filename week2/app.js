3970c4767c1764bc69c8804cb0a8d776c092cf78
qtwtp123 <f74032015@mail.ncku.edu.tw>
2017-10-18 01:18:25 +0800

    fix

--- week2/app.js
+++ week2/app.js
@@ -3,10 +3,10 @@ var shown=true;
 $(document).click(function() {
   if(shown){
   $('#scan').show();
-  $('#scan').animate({'height': '100%'}, 2000,function(){$('#scan').hide();$('#pic').hide();$('#photo').hide(); $('#scan').css('height','1%');shown=false;$('#pic').css('margin-top','0px');});
+  $('#scan').animate({'height': '100%'}, 2000,function(){$('#scan').hide();$('#pic').hide();$('#photo').hide();$('#table').show();$('#scan').css('height','1%');shown=false;$('#pic').css('margin-top','0px');});
   }  else{
   $('#scan').show();
-  $('#scan').animate({'height': '100%'}, 2000,function(){$('#scan').hide();$('#pic').show();$('#photo').show(); $('#pic').animate({'margin-top': '65px'}, 2000);$('#scan').css('height','1%');shown=true;});
+  $('#scan').animate({'height': '100%'}, 2000,function(){$('#scan').hide();$('#pic').show();$('#photo').show(); $('#table').hide();$('#pic').animate({'margin-top': '65px'}, 2000);$('#scan').css('height','1%');shown=true;});
   }
 
   
